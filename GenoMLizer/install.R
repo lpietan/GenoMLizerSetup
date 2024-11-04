77229e17364183ab66f8f7f421734eeb7c50e8c6
Lucas Pietan <148373645+lpietan@users.noreply.github.com>
2024-11-04 12:29:01 -0600

    Delete GenoMLizer directory

--- GenoMLizer/install.R
+++
@@ -1,6 +0,0 @@
-#!/bin/bash/env Rscript
-if(!require(MachineShop)) install.packages("MachineShop",repos = "http://cran.us.r-project.org")
-if(!require(recipes)) install.packages("recipes",repos = "http://cran.us.r-project.org")
-if(!require(readr)) install.packages("readr",repos = "http://cran.us.r-project.org")
-if(!require(doSNOW)) install.packages("doSNOW",repos = "http://cran.us.r-project.org")
-if(!require(dplyr)) install.packages("dplyr",repos = "http://cran.us.r-project.org")
