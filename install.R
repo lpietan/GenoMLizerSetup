4a87d0d18cfbad6eb9be767b3e977cdabbcb8aa3
Lucas Pietan <148373645+lpietan@users.noreply.github.com>
2024-11-04 13:48:07 -0600

    Update install.R

--- install.R
+++ install.R
@@ -1,6 +1,5 @@
 #!/bin/bash/env Rscript
-if(!require(MachineShop)) install.packages("MachineShop",repos = "http://cran.us.r-project.org")
-if(!require(recipes)) install.packages("recipes",repos = "http://cran.us.r-project.org")
-if(!require(readr)) install.packages("readr",repos = "http://cran.us.r-project.org")
-if(!require(doSNOW)) install.packages("doSNOW",repos = "http://cran.us.r-project.org")
-if(!require(dplyr)) install.packages("dplyr",repos = "http://cran.us.r-project.org")
+if(!require(MachineShop)) install.packages("MachineShop",repos = "https://cloud.r-project.org")
+if(!require(readr)) install.packages("readr",repos = "https://cloud.r-project.org")
+if(!require(doSNOW)) install.packages("doSNOW",repos = "https://cloud.r-project.org")
+if(!require(dplyr)) install.packages("dplyr",repos = "https://cloud.r-project.org")
